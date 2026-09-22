4ff801f8c99915701e47abc66edae72bef03e3d4
ShivanshDahiya5 <shivanshdahiya563@gmail.com>
2026-09-22 21:14:29 +0530

    Add initial Header component with imports

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import {
+  Keyboard,
+  BarChart3,
+  Volume2,
+  VolumeX,
+  Palette,
+  Settings,
+  FileText,
+  Sparkles,
+  Check,
+  Zap,
+} from 'lucide-react';
+import { ThemeConfig, ThemeId, UserPreferences } from '../types';
+import { THEMES } from '../utils/themes';
