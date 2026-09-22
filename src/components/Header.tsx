0a918dc33fd8291498d5e30f586f1a2c1d7143e0
ShivanshDahiya5 <shivanshdahiya563@gmail.com>
2026-09-22 21:15:14 +0530

    Add Header component with props and sound toggle functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,3 +13,28 @@ import {
 } from 'lucide-react';
 import { ThemeConfig, ThemeId, UserPreferences } from '../types';
 import { THEMES } from '../utils/themes';
+
+interface HeaderProps {
+  theme: ThemeConfig;
+  preferences: UserPreferences;
+  onUpdatePreferences: (prefs: Partial<UserPreferences>) => void;
+  onOpenHistory: () => void;
+  onOpenCustomText: () => void;
+  onResetTest: () => void;
+}
+
+export const Header: React.FC<HeaderProps> = ({
+  theme,
+  preferences,
+  onUpdatePreferences,
+  onOpenHistory,
+  onOpenCustomText,
+  onResetTest,
+}) => {
+  const [isThemeOpen, setIsThemeOpen] = useState(false);
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+
+  const toggleSound = () => {
+    const nextSound = preferences.sound === 'off' ? 'mechanical' : 'off';
+    onUpdatePreferences({ sound: nextSound });
+  };
